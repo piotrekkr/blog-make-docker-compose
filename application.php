51bbb3c0df0a893f8f4281e3517b58012a5ff3d0
piotrekkr <piotrekkr@users.noreply.github.com>
2023-12-10 16:33:24 +0100

    add draft

--- application.php
+++ application.php
@@ -4,7 +4,6 @@ require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
